99263a0365a255bb4c72fefdbee72aa48b5694b3
Heru Rahmat Aknuari <eyubalzary@gmail.com>
2019-10-15 03:45:53 +0700

    Migration Data

---
+++ database/migrations/2019_10_14_203938_create_options_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateOptionsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('options', function(Blueprint $table)
+		{
+			$table->integer('id', true);
+			$table->string('option_key', 50)->index('option_key');
+			$table->text('option_value', 65535)->nullable();
+			$table->boolean('is_sistem')->default(0);
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('options');
+	}
+
+}
---
+++ database/sql/default.sql
@@ -0,0 +1,20 @@
+INSERT INTO `options` (`id`, `option_key`, `option_value`, `is_sistem`) VALUES
+(1, 'backend_theme', 'adminlte2', 1),
+(2, 'logo', 'logo.jpg', 1),
+(3, 'favicon', 'favicon.jpg', 1),
+(4, 'app_name', 'Laraton', 1),
+(5, 'app_version', 'BETA2', 1),
+(6, 'app_year', '2019', 1),
+(7, 'app_footer_custom', '', 1),
+(8, 'company_name', 'Contoh Perusahaan', 1),
+(9, 'company_address', '-', 1),
+(10, 'company_phone', '-', 1),
+(11, 'company_email', '-', 1),
+(12, 'company_fax', '-', 1),
+(13, 'company_web', '-', 1);
+
+INSERT INTO `users` (`id`, `name`, `username`, `email`, `email_verified_at`, `password`, `user_group_id`, `status`, `avatar`, `remember_token`, `last_login`, `created_at`, `updated_at`, `isDeleted`) VALUES
+(1, 'Administrator', 'admin', 'admin@admin.com', NULL, '$2a$08$PQ/1FcXdgcSSzPEF9Bb2AuK2jRIHyaCHAf5/bgG03Uq4YNgxchWzu', 1, 1, NULL, NULL, '2019-10-15 02:55:16', '2019-10-04 20:01:10', '2019-10-14 19:55:16', 0);
+
+INSERT INTO `user_groups` (`id`, `group_name`, `group_value`) VALUES
+(1, 'superadmin', 'Super Administrator');
