dca20eb390e71c654696341c9804ac85623aaf0f
Heru Rahmat Aknuari <eyubalzary@gmail.com>
2019-10-15 03:33:36 +0700

    Module User,User Group and Options

---
+++ app/Models/Core/Options.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Core;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Options extends Model
+{
+    protected $table='options';
+}
---
+++ app/Models/Core/UserGroup.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Core;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserGroup extends Model
+{
+    protected $table='user_groups';
+}
