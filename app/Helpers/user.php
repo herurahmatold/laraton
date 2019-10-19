d9f2e1c4f3f31470c8f96c66ef70987c998007f8
Heru Rahmat Akhnuari <eyubalzary@gmail.com>
2019-10-20 03:52:21 +0700

    Add user_info_custom, user_group_name_custom, user_group_value_custom, user_avatar_custom

--- app/Helpers/user.php
+++ app/Helpers/user.php
@@ -15,6 +15,12 @@ function user_info($output='id')
     }
 }
 
+function user_info_custom($userid,$output='id')
+{
+   $UserLib=new UserLib();
+    return $UserLib->get_user_info($userid,$output);
+}
+
 function user_group_name()
 {
     $user_group_id=user_info('user_group_id');
@@ -26,7 +32,17 @@ function user_group_name()
     }else{
         return redirect()->route('front');
     }
+}
 
+function user_group_name_custom($userid)
+{
+    $user_group_id=user_info_custom($userid,'user_group_id');
+    if(!empty($user_group_id))
+    {
+        $UserLib=new UserLib();
+        $group_name=$UserLib->get_group_info($user_group_id,'group_name');
+        return $group_name;
+    }
 }
 
 function user_group_value()
@@ -40,9 +56,27 @@ function user_group_value()
     }
 }
 
+function user_group_value_custom($userid)
+{
+    $user_group_id=user_info_custom($userid,'user_group_id');
+    if(!empty($user_group_id))
+    {
+        $UserLib=new UserLib();
+        $group_value=$UserLib->get_group_info($user_group_id,'group_value');
+        return $group_value;
+    }
+}
+
 function user_avatar($size='')
 {
     $userid=user_info('id');
+    $UserLib=new UserLib();
+    $avatar=$UserLib->get_user_avatar($userid,$size);
+    return $avatar;
+}
+
+function user_avatar_custom($userid,$size='')
+{
     $UserLib=new UserLib();
     $avatar=$UserLib->get_user_avatar($userid,$size);
     return $avatar;
