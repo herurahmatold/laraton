0b838f8bada7146c124367e7ccc23863817859ea
Heru Rahmat Akhnuari <eyubalzary@gmail.com>
2019-10-21 23:38:51 +0700

    Add com_datepicker helper

--- app/Helpers/component.php
+++ app/Helpers/component.php
@@ -32,3 +32,46 @@ if(!function_exists('com_password'))
 		return $o;
 	}
 }
+
+if(!function_exists('com_datepicker'))
+{
+	function com_datepicker($Name='tanggal',$Placeholder="",$isRequired=FALSE,$DefaultValue="",$CustomClass="",$CustomID="")
+	{
+		$Required='required=""';
+		if($isRequired==FALSE)
+		{
+			$Required='';
+		}
+		if(empty($Placeholder))
+		{
+			$Placeholder="Tanggal";
+		}
+		
+		if(empty($DefaultValue))
+		{
+			$DefaultValue=date_now(FALSE);
+		}
+		
+		if(empty($CustomClass))
+		{
+			$CustomClass='tanggal';
+		}
+		
+		if(empty($CustomID))
+		{
+			$CustomID=$Name;
+		}
+		
+		$o='
+		<div class="input-group">
+		  <input type="text" name="'.$Name.'" id="'.$CustomID.'" class="form-control '.$CustomClass.'" '.$Required.' placeholder="'.$Placeholder.'" value="'.$DefaultValue.'">
+		  <span class="input-group-btn">
+		    <button class="btn btn-default" tabindex="-1" type="button" date-trigger="'.$CustomID.'">
+		    	<i class="fa fa-calendar"></i>
+		    </button>
+		  </span>
+		</div>
+		';
+		return $o;
+	}
+}
