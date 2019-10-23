5f35a071476c6c7f39f3bc226f09d5cbe315eeda
Heru Rahmat Akhnuari <eyubalzary@gmail.com>
2019-10-24 01:03:09 +0700

    Date helper : date_month_name

--- app/Helpers/date.php
+++ app/Helpers/date.php
@@ -161,3 +161,13 @@ if(!function_exists('date_days_month'))
 	    return $numDays;
 	}
 }
+
+if(!function_exists('date_month_name')){
+	function date_month_name($bulan)
+	{
+		$mons = array(1 => "Januari", 2 => "Februari", 3 => "Maret", 4 => "April", 5 => "Mei", 6 => "Juni", 7 => "Juli", 8 => "Agustus", 9 => "September", 10 => "Oktober", 11 => "November", 12 => "Desember");
+
+		$ft= strtr($bulan, $mons);
+		return $ft;
+	}
+}
