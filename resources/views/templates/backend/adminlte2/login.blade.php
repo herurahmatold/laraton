2237d7778b2862b09bb9a312dd09cd04e2333385
Heru Rahmat Akhnuari <eyubalzary@gmail.com>
2019-11-28 01:00:22 +0700

    Add Icon login

--- resources/views/templates/backend/adminlte2/login.blade.php
+++ resources/views/templates/backend/adminlte2/login.blade.php
@@ -1,9 +1,10 @@
-
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="shortcut icon" href="<?= app_favicon(200); ?>" />
   <title>{{$meta_title}}</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
@@ -14,7 +15,7 @@
   @endphp
   <!-- Theme style -->
   <link rel="stylesheet" href="{{lara_theme_dashboard_url()}}adminlte2/dist/css/AdminLTE.min.css">
-  
+
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
@@ -25,68 +26,70 @@
   <!-- Google Font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
+
 <body class="hold-transition login-page">
-<div class="login-box">
-  <div class="login-logo">
-    <a href="{{route('login')}}"><b>{{option_get('app_name')}}</b></a>
-  </div>
-  <!-- /.login-logo -->
-  <div class="login-box-body">
-    <p class="login-box-msg">Sign in to start your session</p>
-
-    <form action="{{route('logindo')}}" method="post">
-    @csrf
-    @if (session('error'))
-    <div class="alert alert-danger">
-        <i class="fa fa-exclamation-triangle"></i> {{ session('error') }}
+  <div class="login-box">
+    <div class="login-logo">
+      <a href="{{route('login')}}"><b>{{option_get('app_name')}}</b></a>
     </div>
-    @endif
+    <!-- /.login-logo -->
+    <div class="login-box-body">
+      <p class="login-box-msg">Sign in to start your session</p>
 
-    @if ($errors->any())
+      <form action="{{route('logindo')}}" method="post">
+        @csrf
+        @if (session('error'))
         <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+          <i class="fa fa-exclamation-triangle"></i> {{ session('error') }}
         </div>
-    @endif
+        @endif
 
-    @if (session('success'))
-    <div class="alert alert-success">
-        {{ session('success') }}
-    </div>
-    @endif
-      <div class="form-group has-feedback">
-        <input type="text" name="username" class="form-control" placeholder="Username" value="{{old('username')}}">
-        <span class="glyphicon glyphicon-user form-control-feedback"></span>
-      </div>
-      <div class="form-group has-feedback">
-        <input type="password" name="password" class="form-control" placeholder="Password">
-        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-      </div>
-      <div class="row">
-        <div class="col-xs-8">
-          <div class="checkbox">
-            <label>
-              <input type="checkbox"> Remember Me
-            </label>
+        @if ($errors->any())
+        <div class="alert alert-danger">
+          <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+          </ul>
+        </div>
+        @endif
+
+        @if (session('success'))
+        <div class="alert alert-success">
+          {{ session('success') }}
+        </div>
+        @endif
+        <div class="form-group has-feedback">
+          <input type="text" name="username" class="form-control" placeholder="Username" value="{{old('username')}}">
+          <span class="glyphicon glyphicon-user form-control-feedback"></span>
+        </div>
+        <div class="form-group has-feedback">
+          <input type="password" name="password" class="form-control" placeholder="Password">
+          <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+        </div>
+        <div class="row">
+          <div class="col-xs-8">
+            <div class="checkbox">
+              <label>
+                <input type="checkbox"> Remember Me
+              </label>
+            </div>
           </div>
+          <!-- /.col -->
+          <div class="col-xs-4">
+            <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+          </div>
+          <!-- /.col -->
         </div>
-        <!-- /.col -->
-        <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-        </div>
-        <!-- /.col -->
-      </div>
-    </form>
+      </form>
+    </div>
+    <!-- /.login-box-body -->
   </div>
-  <!-- /.login-box-body -->
-</div>
-<!-- /.login-box -->
-@php
-echo cdn_jquery();
-echo cdn_bootstrap_js();
-@endphp
+  <!-- /.login-box -->
+  @php
+  echo cdn_jquery();
+  echo cdn_bootstrap_js();
+  @endphp
 </body>
+
 </html>
