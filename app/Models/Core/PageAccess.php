f88b2a515f28cc845b7bfbef41edec6c01848cc1
Heru Rahmat Akhnuari <eyubalzary@gmail.com>
2020-04-18 16:58:20 +0700

    Update Migrate PageAccess

---
+++ app/Models/Core/PageAccess.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Core;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PageAccess extends Model
+{
+    protected $table = 'page_access_laraton';
+    public $timestamps = false;
+}
---
+++ app/Models/Core/PageAccessDetail.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Core;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PageAccessDetail extends Model
+{
+    protected $table = 'page_access_detail_laraton';
+    public $timestamps = false;
+}
